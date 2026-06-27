12329bdc1f09491ef1d2b0d13f957021014ef1f9
harisparavoor <harisparavoor@gmail.com>
2026-06-27 14:42:09 +0530

    backend

--- src/main/java/com/contoso/payroll/EventHandler.java
+++ src/main/java/com/contoso/payroll/EventHandler.java
@@ -44,7 +44,8 @@ public class EventHandler {
 	}
 
 	/**
-	 * Take an {@link Employee} and get the URI using Spring Data REST's {@link EntityLinks}.
+	 * Take an {@link Employee} and get the URI using Spring Data REST's
+	 * {@link EntityLinks}.
 	 *
 	 * @param employee
 	 */
